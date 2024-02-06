be8c0cf946a9d6827d38bd46bce2af26a3f27cba
Cooty <cooty@gmail.com>
2024-02-06 16:00:24 +0200

    add eas config

--- app.config.js
+++ app.config.js
@@ -6,4 +6,11 @@ export default ({ config }) => ({
     resizeMode: "contain",
     backgroundColor: "#000000",
   },
+  expo: {
+    extra: {
+      eas: {
+        projectId: "bfc681c5-e7cd-458e-bcf4-9fdc45704664",
+      },
+    },
+  },
 });
